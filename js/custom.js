1489ce58e752a9ef23c82f278000fc8c9a59bce6
usama samoo <usamasamoo786@gmail.com>
2026-02-12 14:50:49 +0500

    added popup design

--- js/custom.js
+++ js/custom.js
@@ -92,3 +92,17 @@ jQuery(document).ready(function($) {
   }
 
 })();
+
+
+const input = document.querySelector("#phone");
+
+  window.intlTelInput(input,{
+  initialCountry:"auto",
+  geoIpLookup:callback=>{
+  fetch("https://ipapi.co/json")
+    .then(res=>res.json())
+    .then(data=>callback(data.country_code))
+    .catch(()=>callback("us"));
+  },
+  separateDialCode:true
+});
